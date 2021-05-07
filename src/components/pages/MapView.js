e94ce39aacd64974fc1f5bed48f5d2aa9e4421ba
Susanne Nilsson <45233211+kiwiguard@users.noreply.github.com>
2021-05-07 16:21:46 +0200

    added leaflet map

--- src/components/pages/MapView.js
+++ src/components/pages/MapView.js
@@ -2,6 +2,7 @@ import '../../App.css';
 import startMap from '../../images/startMap.jpg';
 import SimilarItems from '../SimilarItems';
 import { Link } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function MapView () {
     return (
@@ -19,18 +20,27 @@ function MapView () {
             </section>
             <div className='map-div'>
 
-                <div id="mapid">
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className='mapid'>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url={startMap}
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
 
-                </div>
-
-                <div className='map-div--image-container'>
+                {/* <div className='map-div--image-container'>
                     <img src={startMap} alt={'map'} className='map-image' useMap='#image-map' />
                     
                     <map name="image-map">
                         <area target="_self" alt="British Islands" title="British Islands" href="http://localhost:3000/mapView2" coords="1465,1266,963,718" shape="rect" />
                         <area target="_self" alt="Scandinavia" title="Scandinavia" href="http://localhost:3000/mapView3" coords="1536,113,2506,882" shape="rect" />
                     </map>
-                </div>
+                </div> */}
             </div> 
         </div>
         <SimilarItems />
